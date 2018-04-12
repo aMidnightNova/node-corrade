0fb696a9134876f8c22e721ed51328777104a1fb
MidnightRift <amvmody@gmail.com>
2018-04-12 17:38:45 +0700

    Fix copy paths

--- copy.js
+++ copy.js
@@ -1,8 +1,6 @@
 
 const fs = require('fs');
 
-console.log(__dirname);
+fs.createReadStream('config.js').pipe(fs.createWriteStream('../../corradeConfig.js'));
 
-fs.createReadStream('config.js').pipe(fs.createWriteStream('corradeConfig.js'));
-
-fs.createReadStream('example.js').pipe(fs.createWriteStream('example.js'));
+fs.createReadStream('example.js').pipe(fs.createWriteStream('../../example.js'));
