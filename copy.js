5ca11be9e0813c7d7f377403da69c1c4d57ebfcd
MidnightRift <amvmody@gmail.com>
2018-04-12 17:31:06 +0700

    Update to include files on install in the project dir.

--- copy.js
+++ copy.js
@@ -1,6 +1,8 @@
 
 const fs = require('fs');
 
+console.log(__dirname);
+
 fs.createReadStream('config.js').pipe(fs.createWriteStream('corradeConfig.js'));
 
 fs.createReadStream('example.js').pipe(fs.createWriteStream('example.js'));
