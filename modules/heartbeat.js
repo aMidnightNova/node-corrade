69226327c1f382554b13ea1292fb0ea608789eac
MidnightRift <midnight@midnightrift.com>
2019-06-01 11:53:24 -0700

    - Fix heartbeat to show proper day hour and minute uptime

--- modules/heartbeat.js
+++ modules/heartbeat.js
@@ -1,5 +1,3 @@
-
-
 let heartbeat = {};
 
 heartbeat.name = 'heartbeat';
@@ -11,16 +9,21 @@ heartbeat.func = function (_this, params) {
         command: 'getheartbeatdata',
         data: 'AverageCPUUsage,AverageRAMUsage,Version,Uptime'
     }).then(function (res) {
-        console.log('killroy', res.data)
         let out = _this.helpers.csv2arr(res.data, 2);
 
         out = _this.helpers.csv2Obj(out.toString(), 4, ['cpu', 'ram', 'version', 'uptime']);
-
-        console.log('out', out[0])
+        
         out = out[0];
 
-        let uptimeArr = out.uptime.split(':')
-        let uptime = 'Days: ' + Math.floor(uptimeArr[0] / 24) + '   Hours: ' + String(uptimeArr[0]) + '   Minutes: ' + String(uptimeArr[1]);
+        let uptimeArr = out.uptime.split(':');
+        let daysAndHours = uptimeArr[0].split('.');
+
+        let _days = daysAndHours[0];
+        let _hours = daysAndHours[1];
+        let _minutes = uptimeArr[1];
+
+
+        let uptime = 'Days: ' + String(_days) + '   Hours: ' + String(_hours) + '   Minutes: ' + String(_minutes);
         let ram = (Number(out.ram) / 1024 / 1024).toFixed(2);
 
         return Promise.resolve(`\nCPU: ${out.cpu}%\nRAM: ${ram} MB\nUptime: ${uptime}\nVersion: ${out.version}`);
